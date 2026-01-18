91cc0640eb68bb64caa41694705e97ad3c94bc60
roimenashe <rmenashe@aerospike.com>
2026-01-18 12:42:43 +0200

    Add missing ScoreId model

---
+++ src/main/java/io/github/roimenashe/model/ScoredId.java
@@ -0,0 +1,12 @@
+package io.github.roimenashe.model;
+
+public class ScoredId {
+    public final String id;
+    public double score;
+
+    public ScoredId(String id, double score) {
+        this.id = id;
+        this.score = score;
+    }
+}
+
